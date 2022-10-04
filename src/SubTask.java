97a5c25eb6608f0ac256db981243635dd793c53b
MaxiBit1 <Maxvas324@yandex.ru>
2022-10-04 23:23:34 +0300

    feat: добавление описания подзадачи

---
+++ src/SubTask.java
@@ -0,0 +1,18 @@
+/**
+ * Класс подзадач
+ * @author Max Vasilyev
+ * @version 1.0
+ */
+public class SubTask extends Task {
+
+    /** Переопределение метода toString() */
+    @Override
+    public String toString() {
+        return "Subtask {" +
+                "title='" + super.getTitle() + '\'' +
+                ", description='" + super.getDescription() + '\'' +
+                ", status='" + super.getStatus() + '\'' +
+                '}';
+    }
+
+}
