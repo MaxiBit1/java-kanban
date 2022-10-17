761d3c6cda0f1e7ddfeaf4cf4f598ad5ba7434f4
MaxiBit1 <Maxvas324@yandex.ru>
2022-10-17 12:30:45 +0300

    fix: добавлена сигнатура метода историй

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -4,7 +4,7 @@ import data.Epic;
 import data.SubTask;
 import data.Task;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Интерфейс для классов, реализующих логику менеджера
@@ -12,19 +12,19 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public interface TaskManager {
-    ArrayList<Task> getStorageTask();
-    ArrayList<Task> getStorageEpic();
-    ArrayList<Task> getStorageSubtask();
+    List<Task> getStorageTask();
+    List<Task> getStorageEpic();
+    List<Task> getStorageSubtask();
     void createTasks(Task task);
     void createEpic(Epic epic);
     void createSubtacks(SubTask subTask);
-    ArrayList<SubTask> getAllSubtasks(Epic epic);
+    List<SubTask> getAllSubtasks(Epic epic);
     void deleteAllTasks();
     void deleteAllEpics();
     void deleteAllSubtasks();
-    void deletePerIndificationTask(int indification);
-    void deletePerIndificationEpic(int indification);
-    void deletePerIndificationSubtask(int indification);
+    void deleteTaskById(int indification);
+    void deleteEpicById(int indification);
+    void deleteSubtaskById(int indification);
     void setEpicStatus(Epic epic);
     void setUpdateTask(Task task);
     void setUpdateEpic(Epic epic);
@@ -32,4 +32,5 @@ public interface TaskManager {
     Task getTask(int id);
     Task getEpic(int id);
     Task getSubtask(int id);
+    List<Task> getHistory();
 }
