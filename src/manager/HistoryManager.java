ed8bd1d0edc89f7d9557deff886d44d32a1f3423
MaxiBit1 <Maxvas324@yandex.ru>
2022-11-07 21:34:36 +0300

    Sprint 5 7/11/2022

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -5,11 +5,12 @@ import data.Task;
 import java.util.List;
 
 /**
- * Интерфейс для классов, реализующих историю просмотра задач
+ * Интерфейс для классов, реализующих историю просмотра и удаления из истории просмотра задач
  * @author Max Vasilyev
  * @version 1.0
  */
 public interface HistoryManager {
     void add(Task task);
     List<Task> getHistory();
+    void remove(int id);
 }
